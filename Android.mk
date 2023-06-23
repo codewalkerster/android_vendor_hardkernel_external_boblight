0f5a1d19684030bda214215f739dffe7e2fe8cd2
Luke Go <sangch.go@gmail.com>
2023-06-23 10:45:17 +0900

    boblight: android: Changed config script to Android.mk.
    Signed-off-by: Luke Go <sangch.go@gmail.com>
    Change-Id: I6401c7d090b19382e236c510e1823e032a64d62e

--- Android.mk
+++ Android.mk
@@ -1,3 +1,23 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(call all-subdir-makefiles)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := boblight.conf
+LOCAL_SRC_FILES := boblight.conf
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT)/etc
+
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := ws2801.conf
+LOCAL_SRC_FILES := conf/ws2801.conf
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT)/etc
+
+include $(BUILD_PREBUILT)
